655c5278568e7c4904cdc5725e119cc881cfbabe
Fran Agulto <francis.agulto@wpengine.com>
2023-07-09 15:49:20 -0500

    added fixed syntax

--- tests/wpunit/fran-field-test.php
+++ tests/wpunit/fran-field-test.php
@@ -27,9 +27,10 @@ class FranFieldTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCase {
       } 
     }
     ';
-    $actual=graphql([
+    $this->graphql([
       'Query'=>$query
-    ]);
+    ])
+  ;
     //assertQuerySuccessful is a function that comes from WPGraphQL Test Case library
     //expectedNode is a function from the library too that runs when you expect a certain node
     self::assertQuerySuccessful($actual, [
